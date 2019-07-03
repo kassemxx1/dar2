c2bb0f9c4d8b734897bb3a58d391ba96c671915f
kassemx1 <ksmmariam0103>
2019-07-03 15:25:49 +0300

    Initial commit

---
+++ lib/Welcome_Screen.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:dar/Rounded_Button.dart';
+import 'package:dar/constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+class WelcomeScreen extends StatefulWidget {
+  static const String id = 'welcome_screen';
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  final _auth = FirebaseAuth.instance;
+  String email;
+  String password;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(title: Center(
+        child: Text('Dar',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            ),
+          ),
+       ),
+      ),
+
+      body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              textAlign: TextAlign.center,
+              onChanged: (value) {
+
+              },
+              decoration: KTextFieldImputDecoration.copyWith(hintText: 'Enter your email'),
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            TextField(
+                obscureText: true,
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+
+                },
+                decoration:KTextFieldImputDecoration.copyWith(hintText: 'Enter Your Password')
+            ),
+            SizedBox(
+              height: 24.0,
+            ),
+            RoundedButton(colour: Colors.blueAccent,title: 'LogIn',
+              onPressed: ()async{
+                try {
+                  final user = await _auth.signInWithEmailAndPassword(
+                      email: email, password: password);
+                  if (user != null) {
+                    Navigator.pushNamed(context, ChatScreen.id);
+                  }
+                }
+                catch(e){
+                  print(e);
+                }
+              },)
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:dar/Welcome_Screen.dart';
+import 'Main_screen.dart';
+void main() => runApp(Dar());
+
+class Dar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        initialRoute: WelcomeScreen.id,
+        routes: {
+          WelcomeScreen.id: (context) => WelcomeScreen(),
+//          LoginScreen.id : (context) => LoginScreen(),
+//          ChatScreen.id :(context) => ChatScreen(),
+//          RegistrationScreen.id : (context) => RegistrationScreen(),
+        }
+    );
+  }
+}
+
+
